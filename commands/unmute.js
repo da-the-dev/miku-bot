af50e9deeff6e7be297fea5b66e644811dfcde41
da-the-dev <leshiktk@me.com>
2021-03-03 09:55:04 +0300

    Added .unmute

---
+++ commands/unmute.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js')
+const roles = require('../roles.json')
+const embeds = require('../embeds')
+const redis = require('redis')
+
+module.exports =
+    /**
+     * @param {Array<string>} args Command argument
+     * @param {Discord.Message} msg Discord message object
+     * @param {Discord.Client} client Discord client object
+     * @description Usage: .unmute <member>
+     */
+    async (args, msg, client) => {
+        var moderatorRole = msg.guild.roles.cache.get(roles.moder)
+        if(msg.member.roles.cache.find(r => r.position >= moderatorRole.position && r.id != roles.star)) {
+            var mMember = msg.mentions.members.first()
+            if(!mMember) {
+                msg.channel.send(embeds.error(msg.member, 'Вы не указали пользователя для мута!'))
+            }
+
+            // Get and prematurely delete the shadow key
+            const rClient = redis.createClient(process.env.RURL)
+            rClient.get('muted-' + mMember.user.id + '-' + msg.guild.id, (err, res) => {
+                if(err)
+                    console.error(err)
+
+                if(res) {
+                    // Delete the shadow key
+                    rClient.DEL('muted-' + mMember.user.id + '-' + msg.guild.id, err => {
+                        if(err) {
+                            console.error(err)
+                            return
+                        }
+                    })
+
+                    // Shadow key guaranties that userData exists
+                    rClient.get(mMember.user.id, (err, res) => {
+                        if(err)
+                            console.error(err)
+
+                        if(res) {
+                            // Update it
+                            var userData = JSON.parse(res)
+                            var mute = userData[msg.guild.id].mute
+                            if(mute)
+                                delete userData[msg.guild.id].mute
+                            rClient.set(mMember.user.id, JSON.stringify(userData), err => {
+                                if(err)
+                                    console.error(err)
+                                rClient.quit()
+                            })
+                        }
+                    })
+                } else {
+                    msg.reply('not muted in the first place')
+                }
+            })
+        } else {
+            msg.channel.send(embeds.error(msg.member, 'У Вас нет прав для этой команды!'))
+        }
+    }
