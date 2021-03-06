fc7adf8ab8a52c777b20a1873732ecb16f712530
da-the-dev <leshiktk@me.com>
2021-03-06 22:42:16 +0300

    test is test

--- commands/test.js
+++ commands/test.js
@@ -1,13 +1,14 @@
 const anticrash = require('../anti-crash')
 const Discord = require('discord.js')
+const redis = require('redis')
 module.exports =
     /**
     * @param {Array<string>} args Command argument
     * @param {Discord.Message} msg Discord message object
     * @param {Discord.Client} client Discord client object
-    * @description Usage: .warn <member> <reason>
+    * @description Usage: .test <args>
     */
     async (args, msg, client) => {
-        if(msg.author.id == process.env.MYID)
-            anticrash.monitorKicks(msg.guild)
+        if(msg.author.id == process.env.MYID) {
+        }
     }
