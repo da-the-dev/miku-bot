56bb95106382f8c354c887d02e4b1fe83c60dbe8
da-the-dev <leshiktk@me.com>
2021-03-18 13:12:31 +0300

    Added .ugrole

---
+++ commands/ugrole.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js')
+const redis = require('redis')
+const utl = require('../utility')
+module.exports =
+    /**
+    * @param {Array<string>} args Command argument
+    * @param {Discord.Message} msg Discord message object
+    * @param {Discord.Client} client Discord client object
+    * @description Usage: .ugrole
+    */
+    async (args, msg, client) => {
+        const rClient = redis.createClient(process.env.RURL)
+        rClient.get(msg.author.id, (err, res) => {
+            if(err) throw err
+            if(res) {
+                var userData = JSON.parse(res)
+                if(userData.gameRoles == undefined)
+                    userData.gameRoles = false
+                else if(userData.gameRoles == false)
+                    userData.gameRoles = true
+                else if(userData.gameRoles == true)
+                    userData.gameRoles = false
+
+                rClient.set(msg.author.id, JSON.stringify(userData), err => { if(err) throw err })
+                utl.embed(msg, `Игровые роли ${userData.gameRoles ? '**включены**' : '**выключены**'}`)
+            } else {
+                rClient.set(msg.author.id, JSON.stringify({ gameRoles: false }), err => { if(err) throw err })
+                utl.embed(msg, `Игровые роли **выключены**`)
+            }
+        })
+    }
--- utility/gameRoles.js
+++ utility/gameRoles.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
-const gameNames = ["Dota 2", "Counter-Strike: Global Offensive", "PUBG", "Osu!", "League of Legends", "Valorant", "Among Us", "Minecraft", "Brawlhalla", "Apex", "Rainbow Six Siege", "Genshin Impact", "Code"]
+const gameNames = ["Dota 2", "Counter-Strike: Global Offensive", "PUBG", "Osu!", "League of Legends", "Valorant", "Among Us", "Minecraft", "Brawlhalla", "Apex", "Rainbow Six Siege", "Genshin Impact", "Fortnite"]
+const redis = require('redis')
 const constants = require('../constants.json')
 /**
  * Assing an activity role to member
@@ -8,8 +9,22 @@ const constants = require('../constants.json')
  */
 module.exports = (oldPresence, newPresence) => {
     var act = newPresence.activities.find(a => gameNames.includes(a.name))
-    if(act)
-        newPresence.member.roles.add(constants.gameRoles[act.name])
-    else
-        newPresence.member.roles.remove(constants.gameRolesArray)
+    if(!newPresence.user.bot) {
+        if(act) { // If newPresence includes games
+            const rClient = redis.createClient(process.env.RURL)
+            rClient.get(newPresence.userID, (err, res) => {
+                if(err) throw err
+                if(res) {
+                    var userData = JSON.parse(res)
+                    if(userData.gameRoles)
+                        newPresence.member.roles.add(constants.gameRoles[act.name])
+                    else
+                        newPresence.member.roles.remove(constants.gameRolesArray)
+                    rClient.quit()
+                } else
+                    newPresence.member.roles.remove(constants.gameRolesArray)
+            })
+        } else
+            newPresence.member.roles.remove(constants.gameRolesArray)
+    }
 }
