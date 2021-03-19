5cc7aae56360bee534a13ec68b9b2fa07eaf52e2
da-the-dev <leshiktk@me.com>
2021-03-19 22:01:25 +0300

    Added time

--- commands/info.js
+++ commands/info.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const utl = require('../utility')
 
 /**@param {Discord.Client} */
 const getTime = (client) => {
@@ -25,6 +26,19 @@ const getTime = (client) => {
     return hours + ':' + minutes + ':' + seconds
 }
 
+/**
+ * Calculate time when the message was sent
+ * @param {Discord.Message} msg
+ */
+const calculateTime = (msg) => {
+    var time = 'Сегодня, в '
+    var offset = msg.createdAt.getTimezoneOffset() + 180
+    var hours = (msg.createdAt.getHours() + offset / 60).toString().padStart(2, '0')
+    var minutes = msg.createdAt.getMinutes().toString().padStart(2, '0')
+    time += `${hours}:${minutes}`
+    return time
+}
+
 module.exports =
     /**
      * @param {Array<string>} args Command argument
@@ -35,14 +49,16 @@ module.exports =
     (args, msg, client) => {
         var memoryUsed = process.memoryUsage().heapUsed / 1024 / 1024
 
-        var info = new Discord.MessageEmbed()
-            .setColor('#2F3136')
-            .setImage("https://media.discordapp.net/attachments/810255515854569472/815159681562443776/ezgif-6-2cfc121bf785.gif")
-            .addField('Author', `\`\`\`${client.users.cache.find(u => u.id == process.env.MYID).tag}\`\`\``, true)
-            .addField('Prefix', '```.```', true)
-            .addField('RAM usage', `\`\`\`${memoryUsed.toFixed(2)} MB\`\`\``, true)
-            .addField('Prog. Lang.', '```JavaScript```', true)
-            .addField('Ping', `\`\`\`${Math.floor(client.ws.ping)}\`\`\``, true)
-            .addField('Uptime', `\`\`\`${getTime(client)}\`\`\``, true)
+        var info =
+            new Discord.MessageEmbed()
+                .setColor('#2F3136')
+                .setImage("https://cdn.discordapp.com/attachments/814883788369690714/822537373891166248/shirobot_.png")
+                .addField('Author', `\`\`\`${client.users.cache.find(u => u.id == process.env.MYID).tag}\`\`\``, true)
+                .addField('Prefix', '```.```', true)
+                .addField('RAM usage', `\`\`\`${memoryUsed.toFixed(2)} MB\`\`\``, true)
+                .addField('Prog. Lang.', '```JavaScript```', true)
+                .addField('Ping', `\`\`\`${Math.floor(client.ws.ping)}\`\`\``, true)
+                .addField('Uptime', `\`\`\`${getTime(client)}\`\`\``, true)
+                .setFooter(`${msg.author.tag} • ${calculateTime(msg)}`, msg.author.avatarURL())
         msg.channel.send(info)
     }
