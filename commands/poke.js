3907667859074000b15c520d5a3407059dc86c21
MrAnonim <54703955+MrAnonim123@users.noreply.github.com>
2021-03-24 18:37:19 +0300

    Create poke.js

---
+++ commands/poke.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js')
+const utl = require('../utility')
+
+module.exports =
+    /**
+    * @param {Array<string>} args Command argument
+    * @param {Discord.Message} msg Discord message object
+    * @param {Discord.Client} client Discord client object
+    * @description Usage: .kiss <member>
+    */
+    (args, msg, client) => {
+        var mMember = msg.mentions.members.first()
+        if(!mMember)
+            return
+        if(mMember.id == msg.member.id)
+            return
+
+        var name1 = msg.member.nickname
+        if(!name1)
+            name1 = msg.author.username
+        var name2 = mMember.nickname
+        if(!name2)
+            name2 = mMember.user.username
+            let request = require('request')
+            request('https://nekos.life/api/v2/img/poke',(err,res,body) =>{
+            let arr = JSON.parse((body))
+        var firstEmbed = new Discord.MessageEmbed()
+            .setDescription(`<@${msg.member.id}> тыкает <@${mMember.id}>`)
+            .setColor('#2F3136')
+            .setTimestamp()
+            .setImage(arr.url)
+        msg.channel.send(firstEmbed)
+    })
+}
+module.exports.allowedInGeneral = true
