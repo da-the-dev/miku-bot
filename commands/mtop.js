c5af4435a64fa61f0dbadc02e17d04568fa0afd0
da-the-dev <leshiktk@me.com>
2021-03-25 10:49:00 +0300

    Fixed tops again

--- commands/mtop.js
+++ commands/mtop.js
@@ -17,55 +17,50 @@ module.exports =
         const scaner = new redisScan(rClient)
         const scan = util.promisify(scaner.scan).bind(scaner)
         const mget = util.promisify(rClient.mget).bind(rClient)
+        const get = util.promisify(rClient.get).bind(rClient)
 
         var bigData = new Map()
         /**@type {Array<string>} */
-        scan('*')
-            .then(keys => {
-                mget(keys)
-                    .then(data => {
-                        for(i = 0; i < keys.length; i++) {
-                            if(JSON.parse(data[i]).money)
-                                bigData.set(keys[i], JSON.parse(data[i]).money)
+
+        get('day')
+            .then(async res => {
+                bigData = new Map(JSON.parse(res))
+
+                bigData = new Map([...bigData.entries()].sort((a, b) => {
+                    if(a[1] > b[1]) return -1
+                    if(a[1] < b[1]) return 1
+                    return 0
+                }))
+
+                var embed = new Discord.MessageEmbed()
+                    .setAuthor('Топ пользователей по текстовому активу', 'https://media.discordapp.net/attachments/810255515854569472/813821208670765057/photodraw.ru-35920.png')
+                    .setColor('#2F3136')
+                    .setFooter(`${msg.author.tag} • ${utl.embed.calculateTime(msg)}`, msg.author.avatarURL())
+
+
+                var bKeys = Array.from(bigData.keys())
+                var bValues = Array.from(bigData.values())
+
+                var counter = 0
+
+                for(i = 0; i < bKeys.length; i++) {
+                    if(counter < topAmount) {
+                        console.log(bKeys[counter])
+                        var member = await msg.guild.members.fetch(bKeys[counter])
+                            .catch(() => { console.log('no member') })
+
+                        if(member) {
+                            var name = member.nickname ? member.nickname : member.user.username
+
+                            embed.addField('\`#.⠀\`', `\`\`\`${counter + 1}.\`\`\``, true)
+                            embed.addField("`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Ник⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`", `\`\`\`${name}\`\`\``, true)
+                            embed.addField("`⠀⠀⠀⠀ Кол-во сообщений⠀⠀⠀⠀ `", `\`\`\`${bValues[counter]}\`\`\``, true)
                         }
+                        counter++
+                    }
+                }
 
-                        bigData = new Map([...bigData.entries()].sort((a, b) => {
-                            if(a[1] > b[1]) return -1
-                            if(a[1] < b[1]) return 1
-                            return 0
-                        }))
-
-                        bigData = new Map(Array.from([...bigData]).slice(0, topAmount))
-
-
-                        var embed = new Discord.MessageEmbed()
-                            .setAuthor('Топ пользователей по балансу', 'https://media.discordapp.net/attachments/810255515854569472/813821208670765057/photodraw.ru-35920.png')
-                            .setColor('#2F3136')
-                            .setFooter(`${msg.author.tag} • ${utl.embed.calculateTime(msg)}`, msg.author.avatarURL())
-
-
-                        var bKeys = Array.from(bigData.keys())
-                        var bValues = Array.from(bigData.values())
-
-                        var desciption = embed.description + '\n\n'
-                        var counter = 0
-                        var successCounter = 0
-
-                        while(successCounter < topAmount) {
-                            var member = msg.guild.members.cache.get(bKeys[counter])
-                            if(member) {
-                                var name = member.nickname ? member.nickname : member.user.username
-
-                                embed.addField('\`#.⠀\`', `\`\`\`${successCounter + 1}.\`\`\``, true)
-                                embed.addField("`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Ник⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`", `\`\`\`${name}\`\`\``, true)
-                                embed.addField("`⠀⠀⠀⠀ Баланс⠀⠀⠀⠀ `", `\`\`\`${bValues[counter]}\`\`\``, true)
-                                successCounter++
-                            }
-                            counter++
-                        }
-
-                        msg.reply(embed)
-                        rClient.quit()
-                    })
+                msg.reply(embed)
+                rClient.quit()
             })
     }
--- commands/vtop.js
+++ commands/vtop.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+
 const redis = require('redis')
 const utl = require('../utility')
 const redisScan = require('node-redis-scan')
@@ -23,58 +24,54 @@ module.exports =
         scan('*')
             .then(keys => {
                 mget(keys)
-                    .then(data => {
+                    .then(async data => {
                         for(i = 0; i < keys.length; i++) {
                             if(JSON.parse(data[i]).voiceTime)
                                 bigData.set(keys[i], JSON.parse(data[i]).voiceTime)
                         }
-                        console.log(bigData)
                         bigData = new Map([...bigData.entries()].sort((a, b) => {
                             if(a[1] > b[1]) return -1
                             if(a[1] < b[1]) return 1
                             return 0
                         }))
 
-                        bigData = new Map(Array.from([...bigData]).slice(0, topAmount))
-
                         var embed = new Discord.MessageEmbed()
                             .setAuthor('Топ пользователей по голосовому онлайну', 'https://media.discordapp.net/attachments/810255515854569472/813821208670765057/photodraw.ru-35920.png')
                             .setColor('#2F3136')
                             .setFooter(`${msg.author.tag} • ${utl.embed.calculateTime(msg)}`, msg.author.avatarURL())
 
+
                         var bKeys = Array.from(bigData.keys())
                         var bValues = Array.from(bigData.values())
 
-                        console.log(bKeys, bValues)
-
-                        var desciption = embed.description + '\n\n'
                         var counter = 0
-                        var successCounter = 0
 
-                        while(successCounter < topAmount) {
-                            var member = msg.guild.members.cache.get(bKeys[counter])
-                            if(member) {
-                                var name = member.nickname ? member.nickname : member.user.username
+                        for(i = 0; i < bKeys.length; i++) {
+                            if(counter < topAmount) {
+                                var member = await msg.guild.members.fetch(bKeys[counter])
+                                    .catch(() => { console.log('no member') })
 
-                                var time = bValues[counter] // Minutes
-                                console.log(time)
-                                var mmD = Math.floor(time / 24 / 60)
-                                var mmH = Math.floor(time / 60) - (mmD * 24)
-                                var mmM = Math.floor(time) - (mmD * 60 * 24 + mmH * 60)
-                                var muteMsg = ''
+                                if(member) {
+                                    var name = member.nickname ? member.nickname : member.user.username
 
-                                if(mmD) muteMsg += mmD.toString() + "d "
-                                if(mmH) muteMsg += mmH.toString() + "h "
-                                if(mmM) muteMsg += mmM.toString() + "m "
+                                    var time = bValues[counter] // Minutes
+                                    console.log(time)
+                                    var mmD = Math.floor(time / 24 / 60)
+                                    var mmH = Math.floor(time / 60) - (mmD * 24)
+                                    var mmM = Math.floor(time) - (mmD * 60 * 24 + mmH * 60)
+                                    var muteMsg = ''
 
-                                embed.addField('\`#.⠀\`', `\`\`\`${successCounter + 1}.\`\`\``, true)
-                                embed.addField("`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Ник⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`", `\`\`\`${name}\`\`\``, true)
-                                embed.addField("`⠀⠀⠀⠀ Время⠀⠀⠀⠀ `", `\`\`\`${muteMsg}\`\`\``, true)
-                                successCounter++
+                                    if(mmD) muteMsg += mmD.toString() + "d "
+                                    if(mmH) muteMsg += mmH.toString() + "h "
+                                    if(mmM) muteMsg += mmM.toString() + "m "
+
+                                    embed.addField('\`#.⠀\`', `\`\`\`${counter + 1}.\`\`\``, true)
+                                    embed.addField("`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Ник⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`", `\`\`\`${name}\`\`\``, true)
+                                    embed.addField("`⠀⠀⠀⠀ Время⠀⠀⠀⠀ `", `\`\`\`${muteMsg}\`\`\``, true)
+                                }
+                                counter++
                             }
-                            counter++
                         }
-
                         msg.reply(embed)
                         rClient.quit()
                     })
