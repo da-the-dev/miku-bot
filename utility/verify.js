d8155988ae4be80720c67dd547bd5224a03f548a
da-the-dev <leshiktk@me.com>
2021-03-26 01:14:17 +0300

    Fixed welcome message

--- utility/verify.js
+++ utility/verify.js
@@ -8,14 +8,13 @@ const constants = require('../constants.json')
 module.exports.verify = async (reaction, user) => {
     if(reaction.message.id == '819295686415482921') {
         console.log(`[VR] Verified user '${user.tag}'`)
-        const constants = require('../constants.json')
-        reaction.message.guild.members.cache.find(m => m.user.id == user.id).roles.remove(reaction.message.guild.roles.cache.get(constants.roles.verify))
-        console.log('verify 1')
+
+        var member = await reaction.message.guild.members.cache.find(m => m.user.id == user.id).roles.remove(reaction.message.guild.roles.cache.get(constants.roles.verify))
         const calculateTime = () => {
             var time = 'Сегодня, в '
-            var offset = reaction.emoji.createdAt.getTimezoneOffset() + 180
-            var hours = (reaction.emoji.createdAt.getHours() + offset / 60).toString().padStart(2, '0')
-            var minutes = reaction.emoji.createdAt.getMinutes().toString().padStart(2, '0')
+            var offset = member.joinedAt.getTimezoneOffset() + 180
+            var hours = (member.joinedAt.getHours() + offset / 60).toString().padStart(2, '0')
+            var minutes = member.joinedAt.getMinutes().toString().padStart(2, '0')
             time += `${hours}:${minutes}`
             return time
         }
@@ -24,12 +23,15 @@ module.exports.verify = async (reaction, user) => {
             .setImage('https://cdn.discordapp.com/attachments/810255515854569472/822563512634966056/welcome01.png')
             .setColor('#2F3136')
             .setFooter(`${user.tag} • ${calculateTime()}`, user.avatarURL())
-        if(user.id != '810942432274415636') {
-            var msg = await reaction.message.guild.channels.cache.get(constants.channels.general).send(`<@${user.id}>`, emb)
-            setTimeout((msg) => {
-                msg.delete()
-            }, 60000, msg)
-        }
+
+        if(user.id != '810942432274415636') { // My second account ID
+            reaction.message.guild.channels.cache.get(constants.channels.general).send(`<@${user.id}>`, emb)
+                .then(m => {
+                    m.delete(60000)
+                })
+        } else // Send my 2 acc's welcome message to dev channel instead
+            reaction.message.guild.channels.cache.get(constants.channels.dev).send(`<@${user.id}>`, emb)
+
     }
 }
 
