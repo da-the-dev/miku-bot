d8cf6e4b1a4f960678d40888311e74be60b008c9
da-the-dev <leshiktk@me.com>
2021-03-26 11:59:28 +0300

    Updated and reworked reports part 1

--- commands/report.js
+++ commands/report.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const redis = require('redis')
+const util = require('util')
 const utl = require('../utility')
 const constants = require('../constants.json')
 module.exports =
@@ -23,32 +24,32 @@ module.exports =
         }
 
         // Reply to report with response message
-        msg.channel.send(
-            new Discord.MessageEmbed()
-                .setDescription(```Жалоба на пользователя ${mMember.user.tag} отправлена на рассмотрение!```)
-                .setAuthor(msg.author.tag, msg.author.avatarURL())
-                .setColor('#2F3136')
-                .setFooter(`Hoteru • Жалобы • ${utl.embed.calculateTime(msg)}`, client.user.avatarURL())
-        )
-
-
-        var description = `Жалоба на пользователя: **${mMember.user.tag}**\nТекст жалобы: *${args.join()}*\n`
-        if(msg.member.voice.channel)
-            description += `[*Голосовой канал пожаловавшегося*](${await (await msg.member.voice.channel.createInvite()).url})\n`
-        if(mMember.voice.channel)
-            description += `[*Голосовой канал виновника*](${await (await mMember.voice.channel.createInvite()).url})\n`
-
-        if(msg.attachments.array().length > 0)
-            description += `**В репорт приложены файлы!**`
-
-        var embed = new Discord.MessageEmbed()
-            .setAuthor(`${msg.author.tag} отправил жалобу!`, msg.author.avatarURL())
-            .setDescription(description)
+        var replyEmbed = new Discord.MessageEmbed()
+            .setDescription(`\`\`\`Жалоба на пользователя ${mMember.user.tag} отправлена на рассмотрение!\`\`\``)
+            .setAuthor(msg.author.tag, msg.author.avatarURL())
             .setColor('#2F3136')
-            .setFooter(`${client.user.tag} • ${utl.embed.calculateTime(msg)}`, client.user.avatarURL())
+            .setFooter(`Hoteru • Жалобы • ${utl.embed.calculateTime(msg)}`, client.user.avatarURL())
+        msg.channel.send(replyEmbed)
 
-        msg.channel.send('', { embed: embed, files: Array.from(msg.attachments.values()) })
+        var reportEmbed = new Discord.MessageEmbed()
+            .setDescription("Жалоба в режиме ожидания, прожмите реакцию, чтобы перейти к рассмотрению.")
+            .setColor('#2F3136')
+            .setAuthor(`${msg.author.tag} • Отправил жалобу `, msg.author.avatarURL())
+            .setFooter(`Report-System • ${utl.embed.calculateTime(msg)}`, client.user.avatarURL())
+
+        msg.channel.send('', { embed: reportEmbed, files: Array.from(msg.attachments.values()) })
             .then(m => {
                 m.react('☑️')
             })
+
+        const rClient = redis.createClient(process.env.RURL)
+        const set = util.promisify(rClient.set).bind(rClient)
+
+        var data = { guilty: mMember.user.tag, description: args.join() }
+        if(msg.member.voice.channel)
+            data.reportVoiceChannel = (await msg.member.voice.channel.createInvite()).url
+        if(mMember.voice.channel)
+            data.guiltyVoiceChannel = (await mMember.voice.channel.createInvite()).url
+
+        set(`report-${msg.author.tag}`, JSON.stringify(data)).then(() => { console.log('OK'); rClient.quit() })
     }
--- utility/reportHandler.js
+++ utility/reportHandler.js
@@ -1,4 +1,7 @@
 const Discord = require('discord.js')
+const redis = require('redis')
+const util = require('util')
+const utl = require('../utility')
 const constants = require('../constants.json')
 /**
  * Handles report assignment with reactions
@@ -6,15 +9,43 @@ const constants = require('../constants.json')
  * @param {Discord.User} user - Reaction's user
  * @param {Discord.Client} client - Bot client
  */
-module.exports.reportAssignmentHandler = (reaction, user, client) => {
-    if(reaction.message.channel.id == constants.channels.reports) {
+
+const calculateTime = (timestamp) => {
+    var time = 'Сегодня, в '
+    var date = new Date(timestamp)
+    var offset = date.getTimezoneOffset() + 180
+    var hours = (date.getHours() + offset / 60).toString().padStart(2, '0')
+    var minutes = date.getMinutes().toString().padStart(2, '0')
+    time += `${hours}:${minutes}`
+    return time
+}
+
+module.exports.reportAssignmentHandler = async (reaction, user, client) => {
+    if(reaction.message.channel.id == constants.channels.dev) {
         if(reaction.emoji.name == "☑️" && user.id != client.user.id) {
-            var oldMessage = reaction.message
-            var attachments = oldMessage.attachments
-            var embed = oldMessage.embeds[0]
-            var description = `**Репорт забрал ${user.tag}!**\n\n` + embed.description
-            embed.setDescription(description)
-            reaction.message.edit('', { embed: embed, files: Array.from(attachments.values()) })
+            const rClient = redis.createClient(process.env.RURL)
+            const get = util.promisify(rClient.get).bind(rClient)
+
+            var name = reaction.message.embeds[0].author.name
+            name = name.slice(0, name.indexOf('•') - 1)
+            console.log(`report-${name}`)
+            var reportInfo = JSON.parse(await get(`report-${name}`))
+            console.log(reportInfo)
+            rClient.del(`report-${reaction.message.embeds[0].author.name}`)
+            rClient.quit()
+
+            var description = `\n:white_small_square: За жалобу взялся(-ась) <@${user.id}>\n`
+            reportInfo.reportVoiceChannel ? description += `:white_small_square: [Канал пожаловавшегося](${reportInfo.reportVoiceChannel})\n` : null
+            reportInfo.guiltyVoiceChannel ? description += `:white_small_square: [Канал виновника](${reportInfo.guiltyVoiceChannel})` : null
+            console.log(description)
+
+            const takenReport = reaction.message.embeds[0]
+            takenReport.setDescription(description)
+            takenReport.setFooter(`Report-System • ${calculateTime(Date.now())}`, client.user.avatarURL())
+            takenReport.addField("`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Жалоба на⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`", `\`\`\`${reportInfo.guilty}\`\`\``, true)
+            takenReport.addField("`⠀⠀⠀⠀⠀⠀⠀⠀⠀Содержимое жалобы⠀⠀⠀⠀⠀⠀⠀⠀⠀`", `\`\`\`${reportInfo.description}\`\`\``, true)
+
+            reaction.message.edit(takenReport)
                 .then(m => {
                     m.reactions.removeAll()
                         .then(async m => {
