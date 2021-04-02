9c1eec15603ba126ff414589b5d00ecf17383c51
da-the-dev <leshiktk@me.com>
2021-04-02 20:57:59 +0300

    Fix

--- utility/yesNoReactionMessage.js
+++ utility/yesNoReactionMessage.js
@@ -4,16 +4,18 @@ const utl = require('../utility')
 /**
  * Reacts to a message with 2 reactions and executes provided functions accordingly
  * @param {Discord.Message} msg - Messsage to react to
+ * @param {string} id - ID of the only user who can react
  * @param {yes} yes - "Yes" funciton
  * @param {*} no - "No" function
  * @param {*} fail - If timer runs out
  */
-module.exports = async (msg, yes, no, fail) => {
+module.exports = async (msg, id, yes, no, fail) => {
     await msg.react('✅')
     await msg.react('❌')
 
     const filter = (reaction, user) => {
-        return ['✅', '❌'].includes(reaction.emoji.name) || user.id == msg.author
+        console.log(user.username, user.id, id)
+        return ['✅', '❌'].includes(reaction.emoji.name) && user.id == id
     }
     msg.awaitReactions(filter, { max: 1, time: 60000, errors: 'time' })
         .then(reactions => {
