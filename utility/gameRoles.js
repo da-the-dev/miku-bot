aa63b916e49a77a10ffaa3dd44557b9fcdd38924
da-the-dev <leshiktk@me.com>
2021-04-21 20:22:00 +0300

    Fix

--- utility/gameRoles.js
+++ utility/gameRoles.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const constants = require('../constants.json')
+const utl = require('../utility')
 /**
  * Assing an activity role to member
  * @param {Discord.Presence} oldPresence
@@ -8,6 +9,14 @@ const constants = require('../constants.json')
 module.exports = (oldPresence, newPresence) => {
     if(!newPresence.user.bot) {
         var pres = newPresence.activities.find(a => a.type == 'PLAYING')
-        pres ? constants.gameRoles[pres.name] ? !newPresence.member.roles.cache.has(constants.gameRoles[pres.name]) ? newPresence.member.roles.add(constants.gameRoles[pres.name]).catch(err => { }) : null : null : null
+        if(pres && constants.gameRoles[pres.name] && !newPresence.member.roles.cache.has(constants.gameRoles[pres.name])) {
+            utl.db.createClient(process.env.MURL).then(db => {
+                db.get(newPresence.member.guild.id, newPresence.member.id).then(userData => {
+                    if(!userData.gameRoles)
+                        newPresence.member.roles.add(constants.gameRoles[pres.name]).catch(err => { })
+                    db.close()
+                })
+            })
+        }
     }
 }
