e0886a6383c82a97e0ba71379dd1c61f6d103de0
da-the-dev <leshiktk@me.com>
2021-04-21 10:33:34 +0300

    Added bug report

---
+++ commands/bug.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js')
+const utl = require('../utility')
+const constants = require('../constants.json')
+module.exports =
+    /**
+    * @param {Array<string>} args Command argument
+    * @param {Discord.Message} msg Discord message object
+    * @param {Discord.Client} client Discord client object
+    * @description Usage: .bug <text>
+    */
+    (args, msg, client) => {
+        args.shift()
+        console.log(args)
+        if(args.length <= 0) {
+            utl.embed(msg, 'Не указано содержание бага!')
+            return
+        }
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle('Новый баг')
+            .setDescription(args.join(' '))
+            .setColor('#42cbf5')
+
+        if(msg.attachments)
+            msg.guild.channels.cache.get(constants.channels.bugs).send('', {
+                embed: embed, files: msg.attachments.map(a => { return { attachment: a.attachment, name: a.name } })
+            })
+        else
+            msg.guild.channels.cache.get(constants.channels.bugs).send(embed)
+        msg.reply('Баг репорт отправлен, спасибо!')
+    }
