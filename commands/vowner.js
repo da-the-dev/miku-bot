915b54dcd9a522a60afa22d6aa6ab61099c363e0
da-the-dev <leshiktk@me.com>
2021-04-23 13:19:09 +0300

    Renamed some commands

--- commands/vowner.js
+++ commands/vowner.js
@@ -7,7 +7,7 @@ module.exports =
      * @param {Array<string>} args Command argument
      * @param {Discord.Message} msg Discord message object
      * @param {Discord.Client} client Discord client object
-     * @description Usage: .v <setting> <value>
+     * @description Usage: .vowner <member>
      */
     async (args, msg, client) => {
         if(msg.member.voice.channel.parentID != constants.categories.privateRooms)
